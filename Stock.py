85b8b6e39920adf1fd27fc4fc083c64998478478
Tarun Panda <tarunpanda2005@gmail.com>
2026-03-05 11:28:58 +0530

    App code

---
+++ Stock.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import yfinance as yf
+from datetime import datetime
+import plotly.graph_objects as go
+
+st.title("Stock Price Analysis")
+st.subheader("Select a stock to analyze its historical price data")
+option = st.selectbox(
+    "Select the stock you want to analyze",
+    ("AAPL", "GOOGL", "MSFT","AMZN","TSLA","META","NVDA",)
+)
+col1,col2 = st.columns(2)
+with col1:
+    sd = st.date_input("Start Date",datetime(2020,1,1))
+with col2:
+    ed = st.date_input("End Date",datetime(2024,1,1))
+
+ticker = yf.Ticker(option)
+ticker_data = ticker.history(start=sd,end = ed)
+
+st.subheader(f"Historical Price Data for {option}")
+st.dataframe(ticker_data.head())
+
+st.subheader("Stock Performance")
+st.line_chart(ticker_data["Close"])
+
+st.subheader("Stock Volume")
+st.bar_chart(ticker_data["Volume"])
+
+info = ticker.info
+col1,col2,col3 = st.columns(3)
+with col1:
+    st.metric(f"{option} Current Price",f"${info['currentPrice']}")
+with col2:
+    st.metric(f"{option}"+" 52 Week High",f"${info['fiftyTwoWeekHigh']}")
+with col3:
+    st.metric(f"{option}"+" 52 Week Low",f"${info['fiftyTwoWeekLow']}")
+
+ticker_data["MA50"] = ticker_data["Close"].rolling(window=50).mean()
+ticker_data["MA200"] = ticker_data["Close"].rolling(window=200).mean()
+
+st.subheader("Moving Averages")
+st.line_chart(ticker_data[["Close", "MA50", "MA200"]])
+
+ticker_data["Daily Returns"] = ticker_data["Close"].pct_change()
+st.subheader("Daily Returns")
+st.line_chart(ticker_data["Daily Returns"])
+
+fig = go.Figure(data=[go.Candlestick(
+    x=ticker_data.index,
+    open=ticker_data['Open'],
+    high=ticker_data['High'],
+    low=ticker_data['Low'],
+    close=ticker_data['Close']
+)])
+
+st.plotly_chart(fig)
+
+
+st.download_button(
+    label="Download Data as CSV",
+    data  = ticker_data.to_csv().encode('utf-8'),
+    file_name = f"{option}_data.csv",
+    mime = "text/csv"
+)
+
+st.subheader("Statistical Summary")
+
+st.write(ticker_data.describe())
+
+st.markdown("---")
+st.markdown(
+    "<center>Developed by <b>Tarun Panda</b> | Stock Price Analysis App</center>",
+    unsafe_allow_html=True
+)
